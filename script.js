8cb9d19f93558b8e95818d4d15fd2cabb9842213
Kaede Chikakiyo <kaede@Kaedes-MacBook-Pro-5.local>
2020-08-24 20:30:03 +0900

    初めてサイトを作ります

---
+++ script.js
@@ -0,0 +1,29 @@
+
+$(function(){
+   $(".header-sp-icon-wrap").click(function(){ /*このクラスをクリックすると中身の動きを行う*/
+        if($(".header-sp-icon-wrap").hasClass("is-open")){
+            $(".header-sp-nav").css("display","none");
+            $(".header-sp-icon-wrap").removeClass("is-open");
+        }
+        else{
+        $(".header-sp-nav").css("display","block");
+        $(".header-sp-icon-wrap").addClass("is-open");/*addClass=クラス追加＋クリックすると追加される*/
+            
+        }
+
+   });
+});
+
+
+var pics_src = new Array("images/menu-icon.png","images/menu-icon_batu.png","images/menu-icon.png");
+var num = 0;
+
+function slideshow(){
+    if (num == 1) {
+        num = 0;
+    }
+    else {
+        num ++;
+    }
+    document.getElementById("mypic").src=pics_src[num];
+}
